fe07ac8f33250db579970f860f4d9b6da11765df
nef007 <kaidalov.007@yandex.ru>
2020-12-19 14:17:30 +0300

    75 React Final Form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form'
+
+const LoginForm = (props) => {
+
+
+    const onSubmit = (values) => {
+        window.alert(JSON.stringify(values, 0, 2))
+    }
+
+
+    return <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+    <form onSubmit={handleSubmit} >
+                <div>
+                     <Field placeholder={"Login"} name={'login'} component="input"/>
+                </div>
+                <div>
+                    <Field placeholder={"Password"}  name={'password'} component="input"/>
+                </div>
+                <div>
+                    <Field type={"checkbox"}  name={'rememberMe'}component="input"/>  remember me
+                </div>
+                <div>
+                    <button >Login</button>
+                </div>
+
+
+        </form>
+        )}
+    />
+
+
+
+}
 
 const Login = (props) => {
 
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginForm/>
+
+    </div>
 }
 
 export default Login;
